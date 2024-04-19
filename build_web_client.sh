b5ca6f43b4bb6a145fb75afa01fdb690118ef749
Zmallwood <zmallwood@proton.me>
2024-04-19 14:02:46 +0200

    Update build_web_client.sh to rename WebClient.html to index.html

--- build_web_client.sh
+++ build_web_client.sh
@@ -2,4 +2,4 @@ source ~/temp/emsdk/emsdk_env.sh &&
 CC=emcc CXX=em++ cmake project/web_client -B ../build_web_client &&
 cd ../build_web_client &&
 emmake make
-#cmake --build . --parallel 6
+mv ../bin_web_client/WebClient.html ../bin_web_client/index.html
