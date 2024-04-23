6822327bcd6b1d68aa3d6b20ac8e007c1eb9f170
Zmallwood <zmallwood@proton.me>
2024-04-23 15:47:19 +0200

    Update tmux command to run game server container with interactive mode in run_server_containers.sh

--- run_server_containers.sh
+++ run_server_containers.sh
@@ -2,7 +2,7 @@ tmux new-session -d -s mySession -n myWindow
 tmux split-window -hf
 tmux send-keys -t mySession:myWindow "sudo docker run -p 80:80 jod.web_client_server:latest" Enter
 tmux select-pane -L
-tmux send-keys -t mySession:myWindow "sudo docker run -p 1238:1238 jod.game_server:latest" Enter
+tmux send-keys -t mySession:myWindow "sudo docker run -ti -p 1238:1238 jod.game_server:latest" Enter
 tmux split-window -hf
 tmux select-pane -L
 tmux attach -t mySession:myWindow
