0e1019b17d001e818306d2da45e4cf0384c4d8f7
Zmallwood <zmallwood@proton.me>
2024-04-24 08:57:21 +0200

    Save

--- projects/web_client/src/Core/Engine/Cursor/CursorStyles.h
+++ projects/web_client/src/Core/Engine/Cursor/CursorStyles.h
@@ -1,5 +1,19 @@
 #pragma once
 
-namespace zw {
-  enum class CursorStyles { Default, TextInput, Hovering, Rotating, Attacking };
+namespace zw
+{
+    /// Available cursor styles, each yielding a different rendered cursor image.
+    enum class CursorStyles
+    {
+        /// Default style.
+        Default,
+        /// When hovering a text input field.
+        TextInput,
+        /// When hovering something clickable.
+        Hovering,
+        /// When rotating the camera in 3D view.
+        Rotating,
+        /// When attack is possible on mouse click.
+        Attacking
+    };
 }
