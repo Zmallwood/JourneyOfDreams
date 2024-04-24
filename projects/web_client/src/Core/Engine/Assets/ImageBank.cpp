3aab6042fba7d26846b1c3889c6d5a2307e998db
Zmallwood <zmallwood@proton.me>
2024-04-24 08:42:26 +0200

    Save

--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -4,74 +4,111 @@ namespace zw
 {
     ImageBank::ImageBank()
     {
+        // Load all images in images path.
         LoadImages();
     }
 
     ImageBank::~ImageBank()
     {
+        // Iterate through all the loaded images.
         for (const auto &img : m_images)
+        // And free every allocated image resource.
             glDeleteTextures(1, &img.second);
     }
 
     GLuint ImageBank::GetImage(const std::string &imageName)
     {
+        // Iterate through all the loaded images.
         for (auto img : m_images)
+            // If its key, being the hash of the image name, equals the hash of the specified name.
             if (img.first == Hash(imageName))
+                // If so, return this image ID.
                 return img.second;
-
+                
+        // No image with the name found, return fail value.
         return -1;
     }
 
     GLuint ImageBank::CreateBlankImage(const std::string &uniqueImageName)
     {
+        // Generate new image resource and get its ID.
         GLuint texID;
         glGenTextures(1, &texID);
+
+        // Insert new image entry with image name hash as key and the new ID as value.
         m_images.insert({ Hash(uniqueImageName), texID });
-        
+
+        // Return the ID of the newly created blank image resource.
         return texID;
     }
 
     void ImageBank::LoadImages()
     {
         using iterator = std::filesystem::recursive_directory_iterator;
-     
+
+        // Create path string to load the images from.
         auto allImagesPath = k_relImagesPath + "/";
      
         for (auto &entry : iterator(allImagesPath))
         {
             auto absPath = entry.path().string();
+
+            // Only handle files with png extenstion.
             if (FileExtension(absPath) != "png")
                 continue;
+
+            // Do the atual loading of the image file.
             auto texID = LoadSingleImage(absPath);
+
+            // Extract its pure name without path or extension.
             auto imageName = FilenameNoExtension(absPath);
+
+            // Insert a new entry into the images storage,
+            // with the image name hash as key and the resource ID as value.
             m_images.insert({ Hash(imageName), texID });
         }
     }
 
     GLuint ImageBank::LoadSingleImage(const std::string &absFilePath)
     {
+        // Will hold the resulting ID for the loaded image file.
         GLuint texID;
-        
+
+        // Get image data from the image file.
         auto surf = IMG_Load(absFilePath.data());
-        
+
+        // We will work with 2D textures.
         glEnable(GL_TEXTURE_2D);
+
+        // Generate a new OpenGL texture and get its ID.
         glGenTextures(1, &texID);
+
+        // Use the newly created OpenGL texture.
         glBindTexture(GL_TEXTURE_2D, texID);
-        
+
+        // Apply necessary texture parameters.
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        
+
+        // Determine image format.
         if (surf->format->BytesPerPixel == 4)
+        // RGBA (with alpha channel)
         {
+            // Transfer image data from SDL surface to OpenGL texture resource.
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surf->pixels);
         }
         else
+        // RGB (without alpha channel)
         {
+            // Transfer image data from SDL surface to OpenGL texture resource.
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGB, GL_UNSIGNED_BYTE, surf->pixels);
         }
-        
+
+        // Free SDL surface resource. Its not needed anymore
+        // as the image data is stored in the OpenGL texture now.
         SDL_FreeSurface(surf);
-        
+
+        // Return the previously generated resource ID.
         return texID;
     }
 }
