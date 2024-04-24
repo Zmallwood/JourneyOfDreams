465bbffcdcad6464b6ea36867951821aa1ba9af7
Zmallwood <zmallwood@proton.me>
2024-04-24 02:45:40 +0200

    Save

--- project/web_client/src/Common/SDLDeleter.h
+++ project/web_client/src/Common/SDLDeleter.h
@@ -11,14 +11,19 @@ namespace zw
     class SDLDeleter
     {
       public:
+        /// Destroy window object.
         void operator()(SDL_Window *window);
 
+        /// Destroy renderer object.
         void operator()(SDL_Renderer *renderer);
 
+        /// Destroy surface object.
         void operator()(SDL_Surface *surface);
 
+        /// Destroy texture object.
         void operator()(SDL_Texture *texture);
 
+        /// Destroy font object.
         void operator()(TTF_Font *font);
     };
 }
--- project/web_client/src/Common/Utilities.h
+++ project/web_client/src/Common/Utilities.h
@@ -4,11 +4,24 @@
 
 namespace zw
 {
+    /// Return canvas size in pixels.
     Size GetCanvasSize();
+
+    /// Get ratio of canvas width/height.
     float GetAspectRatio();
+
+    /// Get file extension from file path.
     std::string FileExtension(const std::string &absPath);
+
+    /// Get file name without extension from file path.
     std::string FilenameNoExtension(const std::string &absPath);
+
+    /// Convert fractal width to corresponding height.
     float ConvertWidthToHeight(float width);
+
+    /// Convert fractal height to corresponding width.
     float ConvertHeightToWidth(float height);
+
+    /// Get mouse position in canvas in pixels.
     PointF GetMousePosition();
 }
