318f760d08519bc8cde489cf43a373acf942f6b6
Zmallwood <zmallwood@proton.me>
2024-04-24 02:41:14 +0200

    Save

--- project/web_client/src/Common/Utilities.cpp
+++ project/web_client/src/Common/Utilities.cpp
@@ -3,67 +3,67 @@
 
 namespace zw
 {
-	Size GetCanvasSize()
-	{
+    Size GetCanvasSize()
+    {    
         // To store dimensions
-	    int w;
-	    int h;
+        int w;
+        int h;
 
         // Use SDL to get window size = canvas size.
-	    SDL_GetWindowSize(_<Graphics>().Window().get(), &w, &h);
-	    
-	    return { w, h };
-	}
+        SDL_GetWindowSize(_<Graphics>().Window().get(), &w, &h);
+     
+        return { w, h };
+    }
 
-	float GetAspectRatio()
-	{
+    float GetAspectRatio()
+    {
         // Get canvas dimensions.
-	    auto canvasSize = GetCanvasSize();
+        auto canvasSize = GetCanvasSize();
 
         // And calculate the ratio between them.
-	    return static_cast<float>(canvasSize.w) / canvasSize.h;
-	}
+        return static_cast<float>(canvasSize.w) / canvasSize.h;
+    }
 
-	std::string FileExtension(const std::string &absPath)
-	{
+    std::string FileExtension(const std::string &absPath)
+    {
         // Find last occurrence of '.' and return the part following it.
-	    return absPath.substr(absPath.find_last_of('.') + 1);
-	}
+        return absPath.substr(absPath.find_last_of('.') + 1);
+    }
 
-	std::string FilenameNoExtension(const std::string &absPath)
-	{
+    std::string FilenameNoExtension(const std::string &absPath)
+    {
         // Find last occurence of '/' and get the part following it.
-	    auto nameWithExt = absPath.substr(absPath.find_last_of('/') + 1);
+        auto nameWithExt = absPath.substr(absPath.find_last_of('/') + 1);
 
         // Then only keep the part preceeding the last occurrence of '.'.
-	    return nameWithExt.substr(0, nameWithExt.find_last_of('.'));
-	}
+        return nameWithExt.substr(0, nameWithExt.find_last_of('.'));
+    }
 
-	float ConvertWidthToHeight(float width)
-	{
+    float ConvertWidthToHeight(float width)
+    {
         // Use aspect ratio to convert from width to corresponding height.
-	    return width * GetAspectRatio();
-	}
+        return width * GetAspectRatio();
+    }
 
-	float ConvertHeightToWidth(float height)
-	{
+    float ConvertHeightToWidth(float height)
+    {
         // Use aspect ratio to convert from height to corresponding width.
-	    return height / GetAspectRatio();
-	}
+        return height / GetAspectRatio();
+    }
 
-	PointF GetMousePosition()
-	{
+    PointF GetMousePosition()
+    {
         // To store mouse coordinates in pixels.
-	    int x;
-	    int y;
+        int x;
+        int y;
 
         // Use SDL to get current mouse coordinates.
-	    SDL_GetMouseState(&x, &y);
+        SDL_GetMouseState(&x, &y);
 
         // Get canvas size.
-	    auto canvasSize = GetCanvasSize();
+        auto canvasSize = GetCanvasSize();
 
         // And use it to convert pixel coordinates to fractal coordinates.
-	    return { static_cast<float>(x) / canvasSize.w, static_cast<float>(y) / canvasSize.h };
-	}
+        return { static_cast<float>(x) / canvasSize.w, static_cast<float>(y) / canvasSize.h };
+    }
 }
