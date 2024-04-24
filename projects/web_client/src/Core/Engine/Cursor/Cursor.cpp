66bf653abd8d4b6ff88a2066ccdf11d460dc3147
Zmallwood <zmallwood@proton.me>
2024-04-24 08:47:30 +0200

    Save

--- projects/web_client/src/Core/Engine/Cursor/Cursor.cpp
+++ projects/web_client/src/Core/Engine/Cursor/Cursor.cpp
@@ -1,24 +1,43 @@
 #include "Cursor.h"
 #include "Core/Engine/Rendering/ImageRendering/ImageRenderer.h"
 
-namespace zw {
-  Cursor::Cursor() {
-    m_ridCursorImage = _<ImageRenderer>().NewImage();
-    SDL_ShowCursor(SDL_DISABLE);
-  }
+namespace zw
+{
+    Cursor::Cursor()
+    {
+        // Allocate render ID for the image of the cursor.
+        m_ridCursorImage = _<ImageRenderer>().NewImage();
 
-  void Cursor::ResetStyle() {
-    m_style = CursorStyles::Default;
-  }
-  
-  void Cursor::Render() {
-    auto mousePos = GetMousePosition();
-    auto cursorWidth = m_cursorSize;
-    auto cursorHeight = ConvertWidthToHeight(m_cursorSize);
-    auto cursorX = mousePos.x - cursorWidth / 2;
-    auto cursorY = mousePos.y - cursorHeight / 2;
-    auto cursorArea = RectF{ cursorX, cursorY, cursorWidth, cursorHeight };
-    auto cursorImage = m_cursorImages.at(m_style);
-    _<ImageRenderer>().DrawImage(m_ridCursorImage, cursorImage, cursorArea);
-  }
+        // Hide default system cursor when mouse is inside the game canvas.
+        SDL_ShowCursor(SDL_DISABLE);
+    }
+
+    void Cursor::ResetStyle()
+    {
+        // Reset the cursor style to Default at beginning of each frame.
+        m_style = CursorStyles::Default;
+    }
+
+    void Cursor::Render()
+    {
+        // Get current mouse position.
+        auto mousePos = GetMousePosition();
+
+        // Determine cursor image dimensions.
+        auto cursorWidth = m_cursorSize;
+        auto cursorHeight = ConvertWidthToHeight(m_cursorSize);
+
+        // Determine cursor image position.
+        auto cursorX = mousePos.x - cursorWidth / 2;
+        auto cursorY = mousePos.y - cursorHeight / 2;
+
+        // Combine position and dimensions into the draw area.
+        auto cursorArea = RectF{ cursorX, cursorY, cursorWidth, cursorHeight };
+
+        // Get the cursor image corresponding to the current cursor style.
+        auto cursorImage = m_cursorImages.at(m_style);
+
+        // Draw the cursor image at the draw area with the resource ID.
+        _<ImageRenderer>().DrawImage(m_ridCursorImage, cursorImage, cursorArea);
+    }
 }
