be59a972731d0d922c651dd46389618383f7e4fe
Zmallwood <zmallwood@proton.me>
2024-04-27 19:38:47 +0200

    Save

--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -98,8 +98,8 @@ namespace zw
         glBindTexture(GL_TEXTURE_2D, texID);
 
         // Apply necessary texture parameters.
-        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
         // // Determine image format.
         if (surf)
