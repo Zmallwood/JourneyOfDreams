eb392e23bfbcbf76419a9c6521bed59fbfa99fb3
Zmallwood <zmallwood@proton.me>
2024-04-27 17:20:47 +0200

    Save

--- projects/web_client/src/Core/Engine/Input/KeyboardInput.cpp
+++ projects/web_client/src/Core/Engine/Input/KeyboardInput.cpp
@@ -4,45 +4,48 @@ namespace zw
 {
     void KeyboardInput::OnKeyPress(SDL_Keycode key)
     {
-        m_pressedKeys.insert(key);
+        // m_pressedKeys.insert(key);
     }
 
     void KeyboardInput::OnKeyRelease(SDL_Keycode key)
     {
-        m_pressedKeys.erase(key);
+        // m_pressedKeys.erase(key);
     }
 
     bool KeyboardInput::AnyKeyIsPressed()
     {
-        return m_pressedKeys.size() > 0;
+        return false;
+        // return m_pressedKeys.size() > 0;
     }
 
     bool KeyboardInput::KeyIsPressed(SDL_Keycode key)
     {
-        return m_pressedKeys.contains(key);
+        return false;
+        // return m_pressedKeys.contains(key);
     }
 
     bool KeyboardInput::KeyHasBeenFiredPickResult(SDL_Keycode key)
     {
-        auto result = m_pressedKeys.contains(key);
-        m_pressedKeys.erase(key);
-        return result;
+        return false;
+        // auto result = m_pressedKeys.contains(key);
+        // m_pressedKeys.erase(key);
+        // return result;
     }
 
     void KeyboardInput::AppendTextInput(const std::string &text)
     {
-        m_textInput += text;
+        // m_textInput += text;
     }
 
     std::string KeyboardInput::PickTextInput()
     {
-        auto result = m_textInput;
-        m_textInput.clear();
+        // auto result = m_textInput;
+        // m_textInput.clear();
         return result;
     }
 
     void KeyboardInput::ClearTextInput()
     {
-        m_textInput.clear();
+        // m_textInput.clear();
     }
 }
