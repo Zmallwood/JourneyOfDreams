67f2767dbd82db2547b1e64bea0f6b468374c099
Zmallwood <zmallwood@proton.me>
2024-04-27 19:35:19 +0200

    Save

--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -102,20 +102,23 @@ namespace zw
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
         // // Determine image format.
-        // if (surf->format->BytesPerPixel == 4)
-        // // RGBA (with alpha channel)
-        // {
-        //     // Transfer image data from SDL surface to OpenGL texture resource.
-        //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-        //                  surf->pixels);
-        // }
-        // else
-        // // RGB (without alpha channel)
-        // {
-        //     // Transfer image data from SDL surface to OpenGL texture resource.
-        //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGB, GL_UNSIGNED_BYTE,
-        //                  surf->pixels);
-        // }
+        if (surf)
+        {
+            if (surf->format->BytesPerPixel == 4)
+            // RGBA (with alpha channel)
+            {
+                // Transfer image data from SDL surface to OpenGL texture resource.
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                             surf->pixels);
+            }
+            else
+            // RGB (without alpha channel)
+            {
+                // Transfer image data from SDL surface to OpenGL texture resource.
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGB, GL_UNSIGNED_BYTE,
+                             surf->pixels);
+            }
+        }
 
         // Free SDL surface resource. Its not needed anymore
         // as the image data is stored in the OpenGL texture now.
