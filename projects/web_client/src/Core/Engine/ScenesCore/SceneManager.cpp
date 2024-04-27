a09ac6712f108a04c7f748c03e396a2f3f3f31e7
Zmallwood <zmallwood@proton.me>
2024-04-27 16:24:43 +0200

    Save

--- projects/web_client/src/Core/Engine/ScenesCore/SceneManager.cpp
+++ projects/web_client/src/Core/Engine/ScenesCore/SceneManager.cpp
@@ -11,11 +11,11 @@ namespace zw
     SceneManager::SceneManager()
     {
         m_scenes.insert({ Hash("ServerConnectScene"), _<ServerConnectScene>() });
-        m_scenes.insert({ Hash("LoginScene"), _<LoginScene>() });
-        m_scenes.insert({ Hash("LoginNetRequestScene"), _<LoginNetRequestScene>() });
-        m_scenes.insert({ Hash("RegisterScene"), _<RegisterScene>() });
-        m_scenes.insert({ Hash("RegisterNetRequestScene"), _<RegisterNetRequestScene>() });
-        m_scenes.insert({ Hash("MainScene"), _<MainScene>() });
+        // m_scenes.insert({ Hash("LoginScene"), _<LoginScene>() });
+        // m_scenes.insert({ Hash("LoginNetRequestScene"), _<LoginNetRequestScene>() });
+        // m_scenes.insert({ Hash("RegisterScene"), _<RegisterScene>() });
+        // m_scenes.insert({ Hash("RegisterNetRequestScene"), _<RegisterNetRequestScene>() });
+        // m_scenes.insert({ Hash("MainScene"), _<MainScene>() });
 
         GoToScene("ServerConnectScene");
     }
