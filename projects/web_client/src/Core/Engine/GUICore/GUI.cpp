5dbd63ee8eae318828f1541f2093f6f726e4e344
Zmallwood <zmallwood@proton.me>
2024-04-27 16:48:49 +0200

    Save

--- projects/web_client/src/Core/Engine/GUICore/GUI.cpp
+++ projects/web_client/src/Core/Engine/GUICore/GUI.cpp
@@ -23,44 +23,44 @@ namespace zw
 
     void GUI::Update()
     {
-        GetWidget<OnScreenKeyboard>("OnScreenKeyboard")->BringToFront();
+        // GetWidget<OnScreenKeyboard>("OnScreenKeyboard")->BringToFront();
 
-        if (_<MouseInput>().LeftButton().Pressed()
-            && !GetWidget<OnScreenKeyboard>("OnScreenKeyboard")->MouseOver())
-        {
-            SetFocusedWidget(nullptr);
-        }
+        // if (_<MouseInput>().LeftButton().Pressed()
+        //     && !GetWidget<OnScreenKeyboard>("OnScreenKeyboard")->MouseOver())
+        // {
+        //     SetFocusedWidget(nullptr);
+        // }
 
-        // Update all child widgets in the same order as they have been added.
-        for (auto &entry : ChildWidgets())
-            entry.widget->Update();
+        // // Update all child widgets in the same order as they have been added.
+        // for (auto &entry : ChildWidgets())
+        //     entry.widget->Update();
 
-        // Destroy all widgets that should be destroyed.
-        DestroyMarkedWidgets();
+        // // Destroy all widgets that should be destroyed.
+        // DestroyMarkedWidgets();
 
-        // Insert new widgets that are waiting to be inserted.
-        InsertWaitingWidgets();
+        // // Insert new widgets that are waiting to be inserted.
+        // InsertWaitingWidgets();
 
-        // Focus next widget on pressing the tab key.
-        if (_<KeyboardInput>().KeyHasBeenFiredPickResult(SDLK_TAB))
-        {
-            FocusNextWidget();
-        }
+        // // Focus next widget on pressing the tab key.
+        // if (_<KeyboardInput>().KeyHasBeenFiredPickResult(SDLK_TAB))
+        // {
+        //     FocusNextWidget();
+        // }
 
-        // Clear all typed text input as it has already been handled by the GUIs widgets.
-        _<KeyboardInput>().ClearTextInput();
+        // // Clear all typed text input as it has already been handled by the GUIs widgets.
+        // _<KeyboardInput>().ClearTextInput();
 
-        if (FocusedWidget() == nullptr)
-        {
-            GetWidget<OnScreenKeyboard>("OnScreenKeyboard")->Hide();
-        }
+        // if (FocusedWidget() == nullptr)
+        // {
+        //     GetWidget<OnScreenKeyboard>("OnScreenKeyboard")->Hide();
+        // }
     }
 
     void GUI::Render()
     {
-        // Render all widgets in the same order as they have been added.
-        for (auto &entry : ChildWidgets())
-            entry.widget->Render();
+        // // Render all widgets in the same order as they have been added.
+        // for (auto &entry : ChildWidgets())
+        //     entry.widget->Render();
     }
 
     std::shared_ptr<GUIWidget> GUI::GetWidget(const std::string &nameIdentifier)
