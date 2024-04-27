b8eb64fac60939105942b56790c42982850fd0d8
Zmallwood <zmallwood@proton.me>
2024-04-27 15:59:26 +0200

    Save

--- projects/web_client/src/Core/Engine/DoOneGameLoopFrame.cpp
+++ projects/web_client/src/Core/Engine/DoOneGameLoopFrame.cpp
@@ -5,12 +5,12 @@ namespace zw
 {
     void DoOneGameLoopFrame()
     {
-        _<Engine>().Reset();
-        _<Engine>().HandleInput();
-        _<Engine>().Update();
-        _<Engine>().UpdateNet();
-        _<Engine>().Render();
-        _<Engine>().UpdatePostRender();
-        _<Engine>().PresentCanvas();
+        // _<Engine>().Reset();
+        // _<Engine>().HandleInput();
+        // _<Engine>().Update();
+        // _<Engine>().UpdateNet();
+        // _<Engine>().Render();
+        // _<Engine>().UpdatePostRender();
+        // _<Engine>().PresentCanvas();
     }
 }
