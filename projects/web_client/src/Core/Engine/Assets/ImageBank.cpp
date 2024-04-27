aff6ee8cb980869c1454d10b30d4f93241d3e431
Zmallwood <zmallwood@proton.me>
2024-04-27 20:15:35 +0200

    Save

--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -83,7 +83,7 @@ namespace zw
     GLuint ImageBank::LoadSingleImage(const std::string &absFilePath)
     {
         IMG_Init(IMG_INIT_PNG);
-        
+
         // Will hold the resulting ID for the loaded image file.
         GLuint texID;
 
@@ -99,38 +99,41 @@ namespace zw
             glEnable(GL_TEXTURE_2D);
 
             // Use the newly created OpenGL texture.
-            glBindTexture(GL_TEXTURE_2D, texID);
-
-            // // Determine image format.
-            if (surf)
+            if (texID)
             {
-                // Apply necessary texture parameters.
-                // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                glBindTexture(GL_TEXTURE_2D, texID);
 
-                if (surf->format)
+                // // Determine image format.
+                if (surf)
                 {
-                    // if (surf->format->BytesPerPixel == 4)
-                    // // RGBA (with alpha channel)
-                    // {
-                    //     // Transfer image data from SDL surface to OpenGL texture resource.
-                    //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA,
-                    //     GL_UNSIGNED_BYTE,
-                    //                  surf->pixels);
-                    // }
-                    // else
-                    // // RGB (without alpha channel)
-                    // {
-                    //     // Transfer image data from SDL surface to OpenGL texture resource.
-                    //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGB,
-                    //     GL_UNSIGNED_BYTE,
-                    //                  surf->pixels);
-                    // }
-                }
+                    // Apply necessary texture parameters.
+                    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
-                // Free SDL surface resource. Its not needed anymore
-                // as the image data is stored in the OpenGL texture now.
-                SDL_FreeSurface(surf);
+                    if (surf->format)
+                    {
+                        // if (surf->format->BytesPerPixel == 4)
+                        // // RGBA (with alpha channel)
+                        // {
+                        //     // Transfer image data from SDL surface to OpenGL texture resource.
+                        //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA,
+                        //     GL_UNSIGNED_BYTE,
+                        //                  surf->pixels);
+                        // }
+                        // else
+                        // // RGB (without alpha channel)
+                        // {
+                        //     // Transfer image data from SDL surface to OpenGL texture resource.
+                        //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGB,
+                        //     GL_UNSIGNED_BYTE,
+                        //                  surf->pixels);
+                        // }
+                    }
+
+                    // Free SDL surface resource. Its not needed anymore
+                    // as the image data is stored in the OpenGL texture now.
+                    SDL_FreeSurface(surf);
+                }
             }
 
             glBindTexture(GL_TEXTURE_2D, 0);
