ce792d0d562e9edcd89c1c6bc25e83a0f7e07466
Zmallwood <zmallwood@proton.me>
2024-04-27 16:26:22 +0200

    Save

--- projects/web_client/src/DefaultTheme/Scenes/Login/LoginScene.cpp
+++ projects/web_client/src/DefaultTheme/Scenes/Login/LoginScene.cpp
@@ -10,28 +10,28 @@ namespace zw
 {
     LoginScene::LoginScene()
     {
-        m_ridBackground = _<ImageRenderer>().NewImage();
-        m_ridLogo = _<ImageRenderer>().NewImage();
-        auto loginPanel = GUI()->AddWidget(std::make_shared<GUIPanel>(
-            RectF{ .x = 0.5f, .y = 0.5f, .w = 0.3f, .h = 0.3f }, GUIAlign::Center));
-        loginPanel->AddWidget(std::make_shared<GUILabel>(PointF{ 0.0f, 0.0f }, "Login"));
-        loginPanel->AddWidget(
-            std::make_shared<GUIButton>(RectF{ 0.17f, 0.2f, 0.1f, 0.05f }, "Login",
-                                        [] { _<SceneManager>().GoToScene("LoginNetRequestScene"); }));
-        loginPanel->AddWidget(std::make_shared<GUIButton>(RectF{ 0.014f, 0.2f, 0.1f, 0.05f }, "Register",
-                                                          []
-                                                          { _<SceneManager>().GoToScene("RegisterScene"); }));
-        loginPanel->AddWidget("UsernameTextBox",
-                              std::make_shared<GUITextBox>(RectF{ 0.05f, 0.05f, 0.18f, 0.05f }));
-        loginPanel->AddWidget(
-            "PasswordTextBox",
-            std::make_shared<GUITextBox>(RectF{ 0.05f, 0.12f, 0.18f, 0.05f }, Colors::Wheat, true));
+        // m_ridBackground = _<ImageRenderer>().NewImage();
+        // m_ridLogo = _<ImageRenderer>().NewImage();
+        // auto loginPanel = GUI()->AddWidget(std::make_shared<GUIPanel>(
+        //     RectF{ .x = 0.5f, .y = 0.5f, .w = 0.3f, .h = 0.3f }, GUIAlign::Center));
+        // loginPanel->AddWidget(std::make_shared<GUILabel>(PointF{ 0.0f, 0.0f }, "Login"));
+        // loginPanel->AddWidget(
+        //     std::make_shared<GUIButton>(RectF{ 0.17f, 0.2f, 0.1f, 0.05f }, "Login",
+        //                                 [] { _<SceneManager>().GoToScene("LoginNetRequestScene"); }));
+        // loginPanel->AddWidget(std::make_shared<GUIButton>(RectF{ 0.014f, 0.2f, 0.1f, 0.05f }, "Register",
+        //                                                   []
+        //                                                   { _<SceneManager>().GoToScene("RegisterScene"); }));
+        // loginPanel->AddWidget("UsernameTextBox",
+        //                       std::make_shared<GUITextBox>(RectF{ 0.05f, 0.05f, 0.18f, 0.05f }));
+        // loginPanel->AddWidget(
+        //     "PasswordTextBox",
+        //     std::make_shared<GUITextBox>(RectF{ 0.05f, 0.12f, 0.18f, 0.05f }, Colors::Wheat, true));
     }
 
     void LoginScene::OnEnter()
     {
-        GUI()->GetWidget<GUITextBox>("UsernameTextBox")->ClearText();
-        GUI()->GetWidget<GUITextBox>("PasswordTextBox")->ClearText();
+        // GUI()->GetWidget<GUITextBox>("UsernameTextBox")->ClearText();
+        // GUI()->GetWidget<GUITextBox>("PasswordTextBox")->ClearText();
     }
 
     void LoginScene::UpdateDerived()
@@ -40,7 +40,7 @@ namespace zw
 
     void LoginScene::RenderDerived()
     {
-        _<ImageRenderer>().DrawImage(m_ridBackground, "DefaultSceneBackground", { 0.0f, 0.0f, 1.0f, 1.0f });
-        _<ImageRenderer>().DrawImage(m_ridLogo, "JourneyOfDreamsLogo", { 0.4f, 0.2f, 0.2f, 0.1f });
+        // _<ImageRenderer>().DrawImage(m_ridBackground, "DefaultSceneBackground", { 0.0f, 0.0f, 1.0f, 1.0f });
+        // _<ImageRenderer>().DrawImage(m_ridLogo, "JourneyOfDreamsLogo", { 0.4f, 0.2f, 0.2f, 0.1f });
     }
 }
