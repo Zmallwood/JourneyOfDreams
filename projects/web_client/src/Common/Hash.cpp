1f3f1b39e7eb5c2f4d9e64dca14cc4d438a07d7e
Zmallwood <zmallwood@proton.me>
2024-04-28 09:56:21 +0200

    Save

--- projects/web_client/src/Common/Hash.cpp
+++ projects/web_client/src/Common/Hash.cpp
@@ -4,9 +4,10 @@ namespace zw
 {
     int Hash(const std::string &text)
     {
-        unsigned long hash = 5381;
-        for (size_t i = 0; i < text.size(); ++i)
-            hash = 33 * hash + (unsigned char)text[i];
-        return static_cast<int>(hash);
+        return 123;
+        // unsigned long hash = 5381;
+        // for (size_t i = 0; i < text.size(); ++i)
+        //     hash = 33 * hash + (unsigned char)text[i];
+        // return static_cast<int>(hash);
     }
 }
