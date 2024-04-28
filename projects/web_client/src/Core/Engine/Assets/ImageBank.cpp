eade3fb1e6a12fc34bf9bdcf614cd9e4bd40a2a7
Zmallwood <zmallwood@proton.me>
2024-04-28 10:12:20 +0200

    Save

--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -1,146 +1,91 @@
 #include "ImageBank.h"
-// #include "Core/Engine/Graphics/Graphics.h"
-#include <filesystem>
 #include "Common/Hash.h"
 #include "Common/Utilities.h"
 #include <SDL_image.h>
+#include <filesystem>
 namespace zw
 {
-    ImageBank::ImageBank() : m_images(std::make_shared<std::map<int, GLuint>>())
-    {
-        // _<Graphics>(); // Touch Graphics to ensure the graphics, with the GL context, is initialized.
-
-        // Load all images in images path.
-        // LoadImages();
-    }
-
-    // ImageBank::~ImageBank()
-    // {
-    //     if (m_images.empty())
-    //         return;
-
-    //     // Iterate through all the loaded images.
-    //     for (const auto &img : m_images)
-    //         // And free every allocated image resource.
-    //         glDeleteTextures(1, &img.second);
-    // }
-
-    // GLuint ImageBank::GetImage(const std::string &imageName)
-    // {
-    //     return GetImage(Hash(imageName));
-    // }
-
-    // GLuint ImageBank::GetImage(int imageNameHash)
-    // {
-    //     // Iterate through all the loaded images.
-    //     for (auto img : *m_images)
-    //         // If its key, being the hash of the image name, equals the hash of the specified name.
-    //         if (img.first == imageNameHash)
-    //             // If so, return this image ID.
-    //             return img.second;
-
-    //     // No image with the name found, return fail value.
-    //     return -1;
-    // }
-
-    // GLuint ImageBank::CreateBlankImage(const std::string &uniqueImageName)
-    // {
-    //     // Generate new image resource and get its ID.
-    //     GLuint texID;
-    //     glGenTextures(1, &texID);
-
-    //     // Insert new image entry with image name hash as key and the new ID as value.
-    //     m_images->insert({ Hash(uniqueImageName), texID });
-
-    //     // Return the ID of the newly created blank image resource.
-    //     return texID;
-    // }
-
     void ImageBank::LoadImages()
     {
-        using iterator = std::filesystem::directory_iterator;
+        std::string absPath = "GroundGrass.png";
 
-        // Create path string to load the images from.
-        auto allImagesPath = k_relImagesPath + "/";
+        auto texID = LoadSingleImage(absPath);
 
-        for (auto &entry : iterator(allImagesPath))
-        {
-            auto absPath = entry.path().string();
+        // Extract its pure name without path or extension.
+        auto imageName = FilenameNoExtension(absPath);
 
-            // Only handle files with png extenstion.
-            if (FileExtension(absPath) != "png")
-                continue;
+        // Insert a new entry into the images storage,
+        // with the image name hash as key and the resource ID as value.
+        m_images->insert({ Hash(imageName), texID });
 
-            // Do the atual loading of the image file.
-            auto texID = LoadSingleImage(absPath);
+        // using iterator = std::filesystem::directory_iterator;
 
-            // Extract its pure name without path or extension.
-            auto imageName = FilenameNoExtension(absPath);
+        // // Create path string to load the images from.
+        // auto allImagesPath = k_relImagesPath + "/";
 
-            // Insert a new entry into the images storage,
-            // with the image name hash as key and the resource ID as value.
-            m_images->insert({ Hash(imageName), texID });
-        }
+        // for (auto &entry : iterator(allImagesPath))
+        // {
+        //     auto absPath = entry.path().string();
+
+        //     // Only handle files with png extenstion.
+        //     if (FileExtension(absPath) != "png")
+        //         continue;
+
+        //     // Do the atual loading of the image file.
+        //     auto texID = LoadSingleImage(absPath);
+
+        //     // Extract its pure name without path or extension.
+        //     auto imageName = FilenameNoExtension(absPath);
+
+        //     // Insert a new entry into the images storage,
+        //     // with the image name hash as key and the resource ID as value.
+        //     m_images->insert({ Hash(imageName), texID });
+        // }
     }
 
     GLuint ImageBank::LoadSingleImage(std::string absFilePath)
     {
-        SDL_Init(SDL_INIT_EVERYTHING);
         IMG_Init(IMG_INIT_PNG);
 
-        // Will hold the resulting ID for the loaded image file.
         GLuint texID;
 
         if (std::filesystem::exists(absFilePath))
         {
-            // Get image data from the image file.
             auto surf = IMG_Load(absFilePath.c_str());
 
-            // // Generate a new OpenGL texture and get its ID.
             glGenTextures(1, &texID);
 
-            // We will work with 2D textures.
             glEnable(GL_TEXTURE_2D);
 
-            // Use the newly created OpenGL texture.
             if (texID)
             {
                 glBindTexture(GL_TEXTURE_2D, texID);
 
-                // // Determine image format.
                 if (surf)
                 {
-                    // Apply necessary texture parameters.
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
                     if (surf->format)
                     {
                         if (surf->format->BytesPerPixel == 4)
-                        // RGBA (with alpha channel)
                         {
-                            // Transfer image data from SDL surface to OpenGL texture resource.
                             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGBA,
                                          GL_UNSIGNED_BYTE, surf->pixels);
                         }
                         else
-                        // RGB (without alpha channel)
                         {
-                            // Transfer image data from SDL surface to OpenGL texture resource.
                             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surf->w, surf->h, 0, GL_RGB,
                                          GL_UNSIGNED_BYTE, surf->pixels);
                         }
                     }
 
-                    // Free SDL surface resource. Its not needed anymore
-                    // as the image data is stored in the OpenGL texture now.
                     SDL_FreeSurface(surf);
                 }
             }
 
             glBindTexture(GL_TEXTURE_2D, 0);
         }
-        // // Return the previously generated resource ID.
         return texID;
     }
 }
--- projects/web_client/src/Core/Engine/Assets/ImageBank.h
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.h
@@ -5,37 +5,16 @@
 
 namespace zw
 {
-    /// Loads and provides images loaded from the file system.
     class ImageBank
     {
       public:
-        /// Initializes bank by loading all images in images folder.
-        ImageBank();
-
-        /// Frees all allocated OpenGL image resources.
-        // Emscripten: destructors appearantly not called => cannot cleanup
-        // ~ImageBank();
-
-        // /// Provides the image with the specified name.
-        // GLuint GetImage(const std::string &imageName);
-
-        // GLuint GetImage(int imageNameHash);
-
-        // /// Creates a blank image resource, given the specified name, and returns its ID.
-        // GLuint CreateBlankImage(const std::string &uniqueImageName);
-
-        /// Load all images from file system in the images path.
         void LoadImages();
 
       private:
-
-        /// Load a single image at the path and return its resource ID.
         GLuint LoadSingleImage(std::string absFilePath);
 
-        /// Internal image storage.
         std::shared_ptr<std::map<int, GLuint>> m_images;
 
-        /// Path to the images folder, relative the applications base path.
         const std::string k_relImagesPath = "images";
     };
 }
