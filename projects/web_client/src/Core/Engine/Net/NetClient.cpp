d37e546ac4f2ac0dfb302da57422fceb700985d5
Zmallwood <zmallwood@proton.me>
2024-05-01 12:13:28 +0200

    Refactor network logic in NetClient class

--- projects/web_client/src/Core/Engine/Net/NetClient.cpp
+++ projects/web_client/src/Core/Engine/Net/NetClient.cpp
@@ -5,29 +5,41 @@ namespace JourneyOfDreams
 {
     NetClient::NetClient() : m_connection(std::make_shared<NetworkConnection>())
     {
+        /*
+        ** Do nothing. */
     }
 
     void NetClient::BeginEstablishConnection()
     {
+        /*
+        ** Try connect. */
         m_connection->InitiateConnectToServer();
     }
 
     EMSCRIPTEN_RESULT NetClient::Send(std::map<std::string, std::string> data)
     {
+        /*
+        ** Forward function call to connection object. */
         return m_connection->SendMessage(data);
     }
 
     void NetClient::Update()
     {
+        /*
+        ** To implement: Update of network logic. */
     }
 
     bool NetClient::Connected()
     {
+        /*
+        ** Getter. */
         return m_connection->Connected();
     }
 
     std::shared_ptr<NetworkConnection> NetClient::Connection() const
     {
+        /*
+        ** Getter. */
         return m_connection;
     }
 }
