42b7e3cf916b0fa2a75856c808e7531eccca5de4
Zmallwood <zmallwood@proton.me>
2024-05-02 16:08:57 +0200

    Refactor Tester1 class, update Utilities.cpp, DoOneGameLoopFrame.cpp, and StdAfx.h in web_client project

--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -1,13 +1,164 @@
 
 
 #include "ImageBank.h"
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <SDL_image.h>
+#include <png.h>
+// #define STB_IMAGE_IMPLEMENTATION
+// #include <SDL_image.h>
 
-//#include <stb_image.h>
+// #include <stb_image.h>
 
 namespace JourneyOfDreams
 {
+    typedef union RGBA
+    {
+        std::uint32_t Colour;
+        struct
+        {
+            std::uint8_t R, G, B, A;
+        };
+    } *PRGB;
+
+    void ReadFromStream(png_structp PngPointer, std::uint8_t *Data, std::size_t Length)
+    {
+        std::ifstream *Stream = reinterpret_cast<std::ifstream *>(png_get_io_ptr(PngPointer));
+        Stream->read(reinterpret_cast<char *>(Data), Length);
+    }
+
+    void LoadPngImage(const char *FilePath, std::vector<RGBA> &Pixels, std::uint32_t &width,
+                      std::uint32_t &height, std::uint16_t &BitsPerPixel)
+    {
+        Pixels.clear();
+        std::fstream hFile(FilePath, std::ios::in | std::ios::binary);
+
+        if (!hFile.is_open())
+        {
+            throw std::invalid_argument("File Not Found.");
+        }
+
+        std::uint8_t Header[18] = { 0 };
+        hFile.read(reinterpret_cast<char *>(&Header), sizeof(Header));
+        hFile.seekg(8, std::ios::beg);
+
+        if (png_sig_cmp(Header, 0, 8))
+        {
+            hFile.close();
+            throw std::invalid_argument("Error: Invalid File Format. Required: Png.");
+        }
+
+        png_structp PngPointer = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+        if (!PngPointer)
+        {
+            hFile.close();
+            throw std::runtime_error("Error: Cannot Create Read Structure.");
+        }
+
+        png_infop InfoPointer = png_create_info_struct(PngPointer);
+        if (!InfoPointer)
+        {
+            hFile.close();
+            png_destroy_read_struct(&PngPointer, nullptr, nullptr);
+            throw std::runtime_error("Error: Cannot Create InfoPointer Structure.");
+        }
+
+        png_infop EndInfo = png_create_info_struct(PngPointer);
+        if (!EndInfo)
+        {
+            hFile.close();
+            png_destroy_read_struct(&PngPointer, &InfoPointer, nullptr);
+            throw std::runtime_error("Error: Cannot Create EndInfo Structure.");
+        }
+
+        if (setjmp(png_jmpbuf(PngPointer)))
+        {
+            hFile.close();
+            png_destroy_read_struct(&PngPointer, &InfoPointer, nullptr);
+            throw std::runtime_error("Error: Cannot Set Jump Pointer.");
+        }
+
+        png_set_sig_bytes(PngPointer, 8);
+        png_set_read_fn(PngPointer, &hFile, ReadFromStream);
+        png_read_info(PngPointer, InfoPointer);
+
+        auto channels = png_get_channels(PngPointer, InfoPointer);
+
+        int bitdepth, colortype, interlacetype;
+        png_get_IHDR(PngPointer, InfoPointer, &width, &height, &bitdepth, &colortype, &interlacetype, nullptr,
+                     nullptr);
+        png_set_interlace_handling(PngPointer);
+        png_set_strip_16(PngPointer);
+        png_set_packing(PngPointer);
+
+        switch (colortype)
+        {
+        case PNG_COLOR_TYPE_GRAY:
+        {
+            png_set_expand_gray_1_2_4_to_8(PngPointer);
+            png_set_gray_to_rgb(PngPointer);
+            png_set_bgr(PngPointer);
+            break;
+        }
+
+        case PNG_COLOR_TYPE_PALETTE:
+        {
+            png_set_palette_to_rgb(PngPointer);
+            if (png_get_valid(PngPointer, InfoPointer, PNG_INFO_tRNS))
+                png_set_tRNS_to_alpha(PngPointer);
+            else
+                png_set_filler(PngPointer, 0xFF, PNG_FILLER_AFTER);
+            png_set_bgr(PngPointer);
+            BitsPerPixel = 24;
+            break;
+        }
+
+        case PNG_COLOR_TYPE_GRAY_ALPHA:
+        {
+            png_set_gray_to_rgb(PngPointer);
+            break;
+        }
+
+        case PNG_COLOR_TYPE_RGB:
+        {
+            png_set_bgr(PngPointer);
+            png_set_filler(PngPointer, 0xFF, PNG_FILLER_AFTER);
+            BitsPerPixel = 24;
+            break;
+        }
+
+        case PNG_COLOR_TYPE_RGBA:
+        {
+            png_set_bgr(PngPointer);
+            BitsPerPixel = 32;
+            break;
+        }
+
+        default:
+            png_destroy_read_struct(&PngPointer, &InfoPointer, nullptr);
+            throw std::runtime_error("Error: Png Type not supported.");
+            break;
+        }
+
+        png_read_update_info(PngPointer, InfoPointer);
+        channels = png_get_channels(PngPointer, InfoPointer);
+
+        // int bitdepth, colortype, interlacetype;
+        png_get_IHDR(PngPointer, InfoPointer, &width, &height, &bitdepth, &colortype, &interlacetype, nullptr,
+                     nullptr);
+
+        Pixels.resize(width * height);
+        std::vector<std::uint8_t *> RowPointers(height);
+        std::uint8_t *BuffPos = reinterpret_cast<std::uint8_t *>(Pixels.data());
+
+        for (size_t I = 0; I < height; ++I)
+        {
+            RowPointers[I] = BuffPos + (I * width * sizeof(RGBA));
+        }
+
+        png_read_image(PngPointer, RowPointers.data());
+        png_read_end(PngPointer, InfoPointer);
+        png_destroy_read_struct(&PngPointer, &InfoPointer, nullptr);
+        hFile.close();
+    }
+
     ImageBank::ImageBank()
     {
         /*
@@ -108,7 +259,7 @@ namespace JourneyOfDreams
         /*
         ** Get image data from the image file. */
         auto surf = LoadImageData(absFilePath.c_str());
-        //auto surf = IMG_Load(absFilePath.c_str());
+        // auto surf = IMG_Load(absFilePath.c_str());
         /*
         ** We will work with 2D textures. */
         glEnable(GL_TEXTURE_2D);
@@ -151,13 +302,43 @@ namespace JourneyOfDreams
 
     SDL_Surface *ImageBank::LoadImageData(const char *filename)
     {
-        int width;
-        int height;
-        int bytesPerPixel;
-        /*
-        ** Read data */
-        void *data = stbi_load(filename, &width, &height, &bytesPerPixel, 0);
+        //         int width;
+        //         int height;
+        //         int bytesPerPixel;
+        //         /*
+        //         ** Read data */
+        //         void *data = stbi_load(filename, &width, &height, &bytesPerPixel, 0);
 
+        //         int pitch;
+        //         /*
+        //         ** Calculate pitch */
+        //         pitch = width * bytesPerPixel;
+        //         pitch = (pitch + 3) & ~3;
+
+        //         int Rmask;
+        //         int Gmask;
+        //         int Bmask;
+        //         int Amask;
+        //         /*
+        //         ** Setup relevance bitmask */
+        // #if SDL_BYTEORDER == SDL_LIL_ENDIAN
+        //         Rmask = 0x000000FF;
+        //         Gmask = 0x0000FF00;
+        //         Bmask = 0x00FF0000;
+        //         Amask = (bytesPerPixel == 4) ? 0xFF000000 : 0;
+        // #else
+        //         int s = (bytesPerPixel == 4) ? 0 : 8;
+        //         Rmask = 0xFF000000 >> s;
+        //         Gmask = 0x00FF0000 >> s;
+        //         Bmask = 0x0000FF00 >> s;
+        //         Amask = 0x000000FF >> s;
+        // #endif
+        std::vector<RGBA> pixels;
+        std::uint32_t  width, height;
+        std::uint16_t bitsperpixel;
+        LoadPngImage(filename, pixels, width, height, bitsperpixel);
+
+        auto bytesPerPixel = bitsperpixel*8;
         int pitch;
         /*
         ** Calculate pitch */
@@ -175,18 +356,22 @@ namespace JourneyOfDreams
         Gmask = 0x0000FF00;
         Bmask = 0x00FF0000;
         Amask = (bytesPerPixel == 4) ? 0xFF000000 : 0;
-#else   
+#else
         int s = (bytesPerPixel == 4) ? 0 : 8;
         Rmask = 0xFF000000 >> s;
         Gmask = 0x00FF0000 >> s;
         Bmask = 0x0000FF00 >> s;
         Amask = 0x000000FF >> s;
 #endif
+
         /*
         ** Create SDL surface from image data */
-        SDL_Surface *surface = SDL_CreateRGBSurfaceFrom(data, width, height, bytesPerPixel * 8, pitch, Rmask,
-                                                        Gmask, Bmask, Amask);
-        //stbi_image_free(data);
+        SDL_Surface *surface = SDL_CreateRGBSurfaceFrom(pixels.data(), width, height, bitsperpixel, pitch,
+                                                        Rmask, Gmask, Bmask, Amask);
+        // SDL_Surface *surface = SDL_CreateRGBSurfaceFrom(data, width, height, bytesPerPixel * 8, pitch,
+        // Rmask,
+        //                                                 Gmask, Bmask, Amask);
+        // stbi_image_free(data);
         /*
         ** If surface creation failed */
         if (!surface)
