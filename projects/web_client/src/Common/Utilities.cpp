2086d7426860c14311b337d5eae84db171247e77
AndreasÅkerberg <eknah@protonmail.com>
2024-05-06 15:39:43 +0200

    Save

--- projects/web_client/src/Common/Utilities.cpp
+++ projects/web_client/src/Common/Utilities.cpp
@@ -8,18 +8,18 @@
 namespace journey_of_dreams
 {
   Size GetCanvasSize() {
-    // To store dimensions in pixels.
+    // 1. To store dimensions in pixels.
+    // 2. Use GLFW to get canvas size.
     int _w;
     int _h;
-    // Use GLFW to get canvas size.
     glfwGetWindowSize(_<Graphics>().Window(), &_w, &_h);
     return {_w, _h};
   }
 
   float GetAspectRatio() {
-    // Get canvas dimensions.
+    // 1. Get canvas dimensions.
+    // 2. And calculate the ratio between them.
     auto _canvasSize = GetCanvasSize();
-    // And calculate the ratio between them.
     auto _aspectRatio = static_cast<float>(_canvasSize.w) / _canvasSize.h;
     return _aspectRatio;
   }
@@ -31,9 +31,9 @@ namespace journey_of_dreams
   }
 
   std::string FilenameNoExtension(const std::string &_absPath) {
-    // Find last occurence of '/' and get the part following it.
+    // 1. Find last occurence of '/' and get the part following it.
+    // 2. Then only keep the part preceeding the last occurrence of '.'.
     auto _nameWithExt = _absPath.substr(_absPath.find_last_of('/') + 1);
-    // Then only keep the part preceeding the last occurrence of '.'.
     auto _fileName = _nameWithExt.substr(0, _nameWithExt.find_last_of('.'));
     return _fileName;
   }
@@ -49,16 +49,16 @@ namespace journey_of_dreams
   }
 
   PointF GetMousePosition() {
+    // 1. Declare variables to store mouse coordinates in pixels.
+    // 2. Use GLFW to get current mouse coordinates.
+    // 3. Get canvas size.
+    // 4. And use it to convert pixel coordinates to fractal coordinates.
     double _xpos, _ypos;
     glfwGetCursorPos(_<Graphics>().Window(), &_xpos, &_ypos);
-    // To store mouse coordinates in pixels.
     int _x;
     int _y;
-    // Use GLFW to get current mouse coordinates.
     glfwGetWindowPos(_<Graphics>().Window(), &_x, &_y);
-    // Get canvas size.
     auto _canvasSize = GetCanvasSize();
-    // And use it to convert pixel coordinates to fractal coordinates.
     auto _mousePosition = PointF{static_cast<float>(_xpos) / _canvasSize.w,
                                  static_cast<float>(_ypos) / _canvasSize.h};
     return _mousePosition;
--- projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
+++ projects/web_client/src/Core/Engine/Assets/ImageBank.cpp
@@ -7,119 +7,120 @@
 namespace journey_of_dreams
 {
   ImageBank::ImageBank() {
-    // load all images in images path
+    // Load all images in images path.
     LoadImages();
   }
 
   ImageBank::~ImageBank() {
-    // iterate through all the loaded images
+    // 1. Iterate through all the loaded images.
+    // 2. Free every allocated image resource
     for (const auto &_img : m_images) {
-      // and free every allocated image resource
       glDeleteTextures(1, &_img.second);
     }
   }
 
   GLuint ImageBank::GetImage(const std::string &_imageName) {
-    // hash the image name and call the other GetImage method
+    // Hash the image name and call the other GetImage method.
     return GetImage(Hash(_imageName));
   }
 
   GLuint ImageBank::GetImage(int _imageNameHash) {
-    // iterate through all the loaded images
+    // 1. Iterate through all the loaded images.
+    // 2.0 If its key, being the hash of the image name, equals
+    // the hash of the specified name.
+    // 3. No image with the name found, return fail value.
     for (auto _img : m_images) {
-      // if its key, being the hash of the image name, equals the hash of the
-      // specified name
       if (_img.first == _imageNameHash) {
         // if so, return this image ID
         return _img.second;
       }
     }
-    // no image with the name found, return fail value
     return -1;
   }
 
   GLuint ImageBank::CreateBlankImage(const std::string &_uniqueImageName) {
-    // generate new image resource and get its ID.and get its ID
+    // 1. Generate new image resource and get its ID.
+    // 2. Insert new image entry with image name hash as key
+    // and the new ID as value.
+    // 3. return the ID of the newly created blank image resource.
     GLuint _texID;
     glGenTextures(1, &_texID);
-    // insert new image entry with image name hash as key and the new ID as
-    // value
     m_images.insert({Hash(_uniqueImageName), _texID});
-    // return the ID of the newly created blank image resource
     return _texID;
   }
 
   void ImageBank::LoadImages() {
+    // 1. Create path string to load the images from.
+    // 2. Only handle files with png extenstion.
+    // 3. Load the current file as an image resource.
+    // 4. Extract its pure name without path or extension.
+    // 5. Insert a new entry into the images storage, with the
+    // image name hash as key and the resource ID as value.
     using iterator = std::filesystem::recursive_directory_iterator;
-    // create path string to load the images from
     auto _allImagesPath = k_relImagesPath + "/";
     for (auto &_entry : iterator(_allImagesPath)) {
       auto _absPath = _entry.path().string();
-      // only handle files with png extenstion
       if (FileExtension(_absPath) != "png") {
         continue;
       }
-      // load the current file as an image resource
       auto _texID = LoadSingleImage(_absPath);
-      // extract its pure name without path or extension
       auto _imageName = FilenameNoExtension(_absPath);
-      // insert a new entry into the images storage, with the image name hash as
-      // key and the resource ID as value
       m_images.insert({Hash(_imageName), _texID});
     }
   }
 
   GLuint ImageBank::LoadSingleImage(const std::string &absFilePath) {
-    // will hold the resulting ID for the loaded image file
+    // 1. Declare variable to hold the resulting ID for the loaded image file.
+    // 2. Get image data from the image file.
+    // 3. We will work with 2D textures.
+    // 4. Generate a new OpenGL texture and get its ID.
+    // 5. Use the newly created OpenGL texture.
+    // 6. Apply necessary texture parameters.
     GLuint _texID;
-    // get image data from the image file
     auto _surf = LoadImageData(absFilePath.c_str());
-    // auto surf = IMG_Load(absFilePath.c_str());
-    // we will work with 2D textures
     glEnable(GL_TEXTURE_2D);
-    // generate a new OpenGL texture and get its ID
     glGenTextures(1, &_texID);
-    // use the newly created OpenGL texture
     glBindTexture(GL_TEXTURE_2D, _texID);
-    // apply necessary texture parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    // if image format is RGBA (with alpha channel)
+    // 1. if image format is RGBA (with alpha channel),
+    // then transfer image data from SDL surface to OpenGL texture resource.
+    // Else if image format is RGB (without alpha channel),
+    // then transfer image data from SDL surface to OpenGL texture resource.
+    // 2. Free SDL surface resource, its not needed anymore as the image data is
+    // stored in the OpenGL texture now.
+    // 3. Return the previously generated resource ID.
     if (_surf->format->BytesPerPixel == 4) {
-      // transfer image data from SDL surface to OpenGL texture resource
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _surf->w, _surf->h, 0, GL_RGBA,
                    GL_UNSIGNED_BYTE, _surf->pixels);
-    }
-    // if image format is RGB (without alpha channel)
-    else {
-      // transfer image data from SDL surface to OpenGL texture resource
+    } else {
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _surf->w, _surf->h, 0, GL_RGB,
                    GL_UNSIGNED_BYTE, _surf->pixels);
     }
-    // free SDL surface resource, its not needed anymore as the image data is
-    // stored in the OpenGL texture now
     SDL_FreeSurface(_surf);
-    // return the previously generated resource ID
     return _texID;
   }
 
   SDL_Surface *ImageBank::LoadImageData(const char *filename) {
+    // 1. Read data.
+    // 2. Calculate pitch.
+    // 3. Setup relevance bitmask.
+    // 4. Create SDL surface from image data.
+    // 5. If surface creation failed, then free image data and return nullptr.
+    // 6. Else if surface creation was successful, return the surface.
     int _width;
     int _height;
     int _bytesPerPixel;
-    // read data
     void *_data = stbi_load(filename, &_width, &_height, &_bytesPerPixel, 0);
     int _pitch;
-    // calculate pitch
     _pitch = _width * _bytesPerPixel;
     _pitch = (_pitch + 3) & ~3;
     int _rMask;
     int _gMask;
     int _bMask;
     int _aMask;
-    // setup relevance bitmask
 #if SDL_BYTEORDER == SDL_LIL_ENDIAN
     _rMask = 0x000000FF;
     _gMask = 0x0000FF00;
@@ -132,17 +133,13 @@ namespace journey_of_dreams
     _bMask = 0x0000FF00 >> s;
     _aMask = 0x000000FF >> s;
 #endif
-    // create SDL surface from image data
     SDL_Surface *_surface =
-        SDL_CreateRGBSurfaceFrom(_data, _width, _height, _bytesPerPixel * 8, _pitch,
-                                 _rMask, _gMask, _bMask, _aMask);
-    // if surface creation failed
+        SDL_CreateRGBSurfaceFrom(_data, _width, _height, _bytesPerPixel * 8,
+                                 _pitch, _rMask, _gMask, _bMask, _aMask);
     if (!_surface) {
-      // free image data
-      //  stbi_image_free(data);
+      stbi_image_free(data);
       return nullptr;
     }
-    // Return the created SDL surface
     return _surface;
   }
 } // namespace journey_of_dreams
